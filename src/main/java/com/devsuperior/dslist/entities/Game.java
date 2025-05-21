eb68b6c44988fa2dbd17ce121fc2290466ff19d1
Rafael Niccioli <r.niccioli@gmail.com>
2025-05-21 19:47:16 -0300

    insere game e descricao

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -15,7 +15,7 @@ public class Game {
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String plataforms;
+    private String platforms;
     private Double score;
     private String imgUrl;
 
@@ -28,13 +28,13 @@ public class Game {
     public Game(){
     }
 
-    public Game(Long id, String title, Integer year, String genre, String plataforms, Double score, String imgUrl,
+    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
                 String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.plataforms = plataforms;
+        this.platforms = platforms;
         this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
@@ -73,12 +73,12 @@ public class Game {
         this.genre = genre;
     }
 
-    public String getPlataforms() {
-        return plataforms;
+    public String getPlatforms() {
+        return platforms;
     }
 
-    public void setPlataforms(String plataform) {
-        this.plataforms = plataform;
+    public void setPlatforms(String plataform) {
+        this.platforms = plataform;
     }
 
     public Double getScore() {
